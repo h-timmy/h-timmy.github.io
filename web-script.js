40c127aa16c1501a8bc85341bc6de97f815db310
h-timmy <33536933+h-timmy@users.noreply.github.com>
2020-09-06 12:53:35 -0400

    Update web-script.js

--- web-script.js
+++ web-script.js
@@ -1,20 +1,29 @@
 //const config = require('./reservations.json')
 
-var fs = require('fs');
+//var fs = require('fs');
 
-const { JSDOM } = require( "jsdom" );
-const { window } = new JSDOM( "" );
-const { document } = (new JSDOM('')).window;
-global.document = document;
-const $ = require( "jquery" )( window );
+//const { JSDOM } = require( "jsdom" );
+//const { window } = new JSDOM( "" );
+//const { document } = (new JSDOM('')).window;
+//global.document = document;
 
 
-var content = fs.readFileSync("reservations.json");
+
+//var content = fs.readFileSync("reservations.json");
 //console.log(content);
 
-var json = JSON.parse(content);
+$(document).ready(function(){
+        $.getJSON("reservations.json", function(data){
+            console.log("A"); // Prints: Harry
+            console.log("B"); // Prints: 14
+        }).fail(function(){
+            console.log("An error has occurred.");
+        });
+    });
 
-console.log(json.reservations[1]);
+//var json = JSON.parse(content);
+
+//console.log(json.reservations[1]);
 
 
 
